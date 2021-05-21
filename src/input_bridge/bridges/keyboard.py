fd1a2e6dc6faace1ca74e66357723c6e7321aeb6
Luke Zulauf <lzulauf@gmail.com>
2021-05-21 11:24:20 -0700

    Add keyboard monitoring

--- src/input_bridge/bridges/keyboard.py
+++ src/input_bridge/bridges/keyboard.py
@@ -1,4 +1,5 @@
 import logging
+import queue
 
 import keyboard
 
@@ -7,10 +8,25 @@ from input_bridge import Bridge
 
 class KeyboardBridge(Bridge):
     def __init__(self):
-        import queue
+        self.events = []
         self.event_queue = queue.Queue()
+        keyboard.start_recording(self.event_queue)
+
+    def __del__(self):
+        keyboard.stop_recording()
 
     def send_keys(self, *keys):
         for key in keys:
             logging.info(f'Pressing key {key}')
             keyboard.send(key)
+
+    def begin_update(self):
+        try:
+            while True:
+                self.events.append(self.event_queue.get(block=False))
+                self.event_queue.task_done()
+        except queue.Empty:
+            pass
+
+    def end_update(self):
+        self.events = []
